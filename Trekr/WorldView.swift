8ea0c0ebae5848ed632625a0b51667d44a1cdca7
Faisal Hussain <faisal@base29.com>
2021-04-06 14:07:30 +0500

    added annotation to map to show country flags

--- Trekr/WorldView.swift
+++ Trekr/WorldView.swift
@@ -20,8 +20,19 @@ struct WorldView: View {
     )
     
     var body: some View {
-        Map(coordinateRegion: $region)
-            .navigationTitle("Locations")
+        Map(coordinateRegion: $region, annotationItems: locations.places){
+            location in
+            MapAnnotation(coordinate: CLLocationCoordinate2D(
+                latitude: location.latitude, longitude: location.longitude)
+            ){
+                Image(location.country)
+                    .resizable()
+                    .cornerRadius(10)
+                    .frame(width: 80, height: 40)
+                    .shadow(radius: 3)
+            }
+        }
+        .navigationTitle("Locations")
     }
 }
 
